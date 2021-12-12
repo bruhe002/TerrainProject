2b79a37e75c9505d7b92add37511a86ffb64b815
Eric Bruhns <bruhe002@gmail.com>
2021-12-12 14:56:02 -0800

    Terrain methods to overridden via Subclasses

--- Terrain.java
+++ Terrain.java
@@ -1,4 +1,10 @@
 public class Terrain
 {
-    Terrain() {}
+    //Terrain() {}
+
+    public double foodCost() {return 1.0;}
+
+    public double waterCost() {return 1.0;}
+
+    public double staminaCost() {return 1.0;}
 }
